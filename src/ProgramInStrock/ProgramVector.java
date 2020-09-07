7967f995ed91f63dc732fac60120cfe1f86b6315
Jose Paulo <paulobrandao356@gmai.com>
2020-09-07 20:30:06 -0300

    Add vector

---
+++ src/ProgramInStrock/ProgramVector.java
@@ -0,0 +1,31 @@
+package ProgramInStrock;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramVector {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		double[] vect=new double[n];
+		
+		for (int i=0; i<n;i++) {
+			vect[i] = scanner.nextDouble();
+			
+		}
+		
+		double sum = 0.0;
+		for (int i = 0; i < vect.length; i++) {
+			sum += vect[i];
+		}
+		
+		double avg = sum / n;
+		
+		System.out.printf("ARAGE HEIGHT: %.2f%n",avg);
+		scanner.close();
+	}
+
+}
